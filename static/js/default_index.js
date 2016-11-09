caca319b9249c7f6a31019b07ccb1c6898935d93
David <daastewa@ucsc.edu>
2016-11-08 18:19:51 -0800

    Better Sidebar implementation

---
+++ static/js/default_index.js
@@ -0,0 +1,37 @@
+// This is the js for the default/index.html view.
+
+
+var app = function() {
+
+    var self = {};
+
+    Vue.config.silent = false; // show all warnings
+
+    self.show = function() {
+
+        alert('Use Vue.js Javascript function here');
+    }
+
+    // Complete as needed.
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+        logged_in: false,
+        },
+        methods: {
+            show: self.show,
+        }
+
+    });
+
+     return self;
+};
+
+$("#vue-div").show();
+var APP = null;
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){APP = app();});
