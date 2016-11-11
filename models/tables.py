34b08bc9ed4c2d1305719349bc9f0c235a395d94
Robert Pence <rpence@ucsc.edu>
2016-11-11 14:42:53 -0800

    Made new branch to add the databases for users and annoucements

---
+++ models/tables.py
@@ -0,0 +1,28 @@
+# Define your tables below (or better in another model file) for example
+#
+# >>> db.define_table('mytable', Field('myfield', 'string'))
+#
+# Fields can be 'string','text','password','integer','double','boolean'
+#       'date','time','datetime','blob','upload', 'reference TABLENAME'
+# There is an implicit 'id integer autoincrement' field
+# Consult manual for more options, validators, etc.
+
+import datetime
+
+def get_user_email():
+    return auth.user.email if auth.user else None
+
+db.define_table('track',
+                Field('artist'),
+                Field('album'),
+                Field('title'),
+                Field('duration', 'float'),
+                Field('rating', 'float'),
+                Field('num_plays', 'integer'),
+                Field('created_by', default=get_user_email()),
+                Field('created_on', default=datetime.datetime.utcnow()),
+                )
+
+
+# after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
