5fcaa26e9cc553eb17fb75a6a0ec484bd6dacdfd
Davan <davan108@yahoo.com>
2016-11-11 18:53:45 -0800

    A stub announcement is now created when clicking on the map

--- static/js/ourmap.js
+++ static/js/ourmap.js
@@ -3,15 +3,51 @@
  */
 
 
+var map_var = "hello from ourmap.js!";
 
-var map = L.map('mapid').setView([36.991, -122.060], 15);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var New_Map = function () {
 
-//        L.marker([36.991, -122.060]).addTo(map)
-//            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//            .openPopup();
+    var map = L.map('mapid').setView([36.991, -122.060], 15);
 
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+
+
+
+    var self = {};
+    self.map = map;
+    self.circle = null;
+
+    self.set_circle = function(circle){
+        self.circle = circle
+    };
+
+
+    self.draw_circle = function (e) {
+        L.circle(e.latlng, self.circle).addTo(self.map);
+
+    };
+
+
+    self.map.on('click', function(e) {
+        if (self.circle != null) {
+            self.draw_circle(e);
+            self.circle = null;
+        }
+    });
+
+
+    //        L.marker([36.991, -122.060]).addTo(map)
+    //            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //            .openPopup();
+
+    /*
+
+    */
+
+    return  self;
+};
 
