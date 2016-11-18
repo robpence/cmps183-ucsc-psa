13aa81aee78a06f0d23001b7034bb33edc50c084
Robert Pence <rpence@ucsc.edu>
2016-11-17 17:35:31 -0800

    I tryed to edit the modal

--- static/js/main_map.js
+++ static/js/main_map.js
@@ -13,19 +13,49 @@ map.on('click', create_announcement);
 L.marker([36.991, -122.060]).addTo(map)
     .bindPopup('This is the center of Campus. <button> Hello </button>');
 
+var map_is_clickable = true;
+
 //This will prompt the user with a modal or something to create a announcement
-function create_announcement(e){
-    console.log("create_announcement was called");
-    var popup = L.popup();
+function create_announcement(e) {
 
-    popup
-        .setLatLng(e.latlng)
-        .setContent('<a data-toggle="modal" href="#CreatePositionModal" class="btn btn-primary">Create Announcement</a>')
-        .openOn(map);
+    if (map_is_clickable == true) {
+        console.log("create_announcement ws called");
 
+        var popup = L.popup();
+        popup
+            .setLatLng(e.latlng)
+            .setContent('<a data-toggle="modal" href="#CreateAnnouncementModal" class="btn btn-primary" >Create Announcement</a>')
+            .openOn(map);
+    }
 
-    //create_announcement_submit(e);
+    //map._handlers.forEach(function(handler) {
+    //    handler.disable();
+    //    });
+
+    create_announcement_submit(e);
 }
+
+function disable_map(){
+    console.log("disable_map was called");
+    map_is_clickable = false;
+    map.dragging.disable();
+    map.touchZoom.disable();
+    map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
+    map.boxZoom.disable();
+    map.keyboard.disable();
+}
+function enable_map(){
+    console.log("enable_map was called");
+    map_is_clickable = true;
+    map.dragging.enable();
+    map.touchZoom.enable();
+    map.doubleClickZoom.enable();
+    map.scrollWheelZoom.enable();
+    map.boxZoom.enable();
+    map.keyboard.enable();
+}
+
 
 //this will be called by a button probably
 function create_announcement_submit(e){
